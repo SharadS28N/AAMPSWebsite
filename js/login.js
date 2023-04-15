b1fd85361130903f7ed0854384f8bc24d51fce37
SharadS28N <sharad.bhandari218@gmail.com>
2023-04-15 22:04:03 +0545

    Message

--- js/login.js
+++ js/login.js
@@ -1,16 +1,1 @@
 particlesJS.load('particles-js', 'particles.json');
-
-$(document).ready(function() {
-	$('form').submit(function(e) {
-		e.preventDefault();
-		let email = $('#email').val();
-		let password = $('#password').val();
-
-		if (email === '' || password === '') {
-			alert('Please fill in all fields');
-		} else {
-			// Do login
-			console.log('Logged in');
-		}
-	});
-});

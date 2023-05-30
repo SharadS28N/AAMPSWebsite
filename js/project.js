c4c4d241fcba0f3769b9a29f905eada26dab69f5
SharadS28N <sharad.bhandari218@gmail.com>
2023-05-30 17:17:31 +0545

    new changes

---
+++ js/project.js
@@ -0,0 +1,17 @@
+class Project extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            name: '',
+            apppversion= '',
+        }
+    }
+    render(){
+        return(
+            <>
+            <p>This are some of my</p>
+            <h2>Projects</h2>
+            </>
+        )
+    }
+}

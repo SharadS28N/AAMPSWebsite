6a4ba26f73f6f052ffd72715061a1e991cc9a2d6
SharadS28N <sharad.bhandari222@gmail.com>
2023-11-11 21:25:43 +0545

    New Changes

---
+++ js/firebase-config.js
@@ -0,0 +1,14 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCP9OERGwYkcOr_DI494CbL2vr6VV94ZQk",
+    authDomain: "logincredentialforwebsite.firebaseapp.com",
+    projectId: "logincredentialforwebsite",
+    storageBucket: "logincredentialforwebsite.appspot.com",
+    messagingSenderId: "959450861298",
+    appId: "1:959450861298:web:eccc9f1bfae89acc418a6b",
+    measurementId: "G-P3L7PJNSYW"
+  };
+  
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+  

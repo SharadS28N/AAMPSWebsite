ff29783134091696a24284f9524d2061a35912df
SharadS28N <sharad.bhandari222@gmail.com>
2023-11-12 10:17:16 +0545

    New Changes

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,6 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-import { getAuth , GoogleAuthProvider, signInWithRedirect  } from "firebase/auth";
+import { getAuth , GoogleAuthProvider, signInWithRedirect, getRedirectResult  } from "firebase/auth";
 
 
 const firebaseConfig = {
@@ -21,4 +21,24 @@ const firebaseConfig = {
 
   google-signin-button.addEventListener('click', (e) => {
     signInWithRedirect(auth, provider);
+    getRedirectResult(auth)
+  .then((result) => {
+    // This gives you a Google Access Token. You can use it to access Google APIs.
+    const credential = GoogleAuthProvider.credentialFromResult(result);
+    const token = credential.accessToken;
+
+    // The signed-in user info.
+    const user = result.user;
+    // IdP data available using getAdditionalUserInfo(result)
+    // ...
+  }).catch((error) => {
+    // Handle Errors here.
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    // The email of the user's account used.
+    const email = error.customData.email;
+    // The AuthCredential type that was used.
+    const credential = GoogleAuthProvider.credentialFromError(error);
+    // ...
+  });
   });
